a715f2b0d47baecddfe5d6f2b041f4babda5f6c7
James Z. Zhang <144994336+James-Z-Zhang00@users.noreply.github.com>
2024-06-18 08:29:33 -0400

    Delete 43_Multiply_Strings directory

--- 43_Multiply_Strings/main.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
-        return str(self._getNumber(num1) * self._getNumber(num2))
-
-    def _getNumber(self, digits: str):
-        num = 0
-        for digit in digits:
-            num = num * 10 + int(digit)
-        return num
